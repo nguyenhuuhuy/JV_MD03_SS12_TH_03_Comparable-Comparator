80b38af9a2dd320c04625324a73d68ea4fd5a7f2
nguyenhuuhuy <huy.cb96@gmail.com>
2023-03-22 07:51:46 +0700

    bt

---
+++ src/ageComparator/AgeComparator.java
@@ -0,0 +1,18 @@
+package ageComparator;
+
+import student.Student;
+
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        if(o1.getAge() > o2.getAge()){
+            return 1;
+        }else if(o1.getAge().equals(o2.getAge())){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+}
